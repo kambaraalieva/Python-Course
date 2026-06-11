c6f87867941ef39d83a5e357469834ffe576500c
MacBook Air M4 <macbookairm4@MacBook-Air-MacBook.local>
2026-06-11 18:12:03 +0600

    Session 4 done

--- Session_4/module_4.py
+++ Session_4/module_4.py
@@ -3,28 +3,48 @@ PASSING_GRADE = 8
 
 class Trainee:
     def __init__(self, name, surname):
-        pass
+        self.name = name
+        self.surname = surname
+        self.visited_lectures = 0
+        self.missed_lectures = 0
+        self.done_home_tasks = 0
+        self.missed_home_tasks = 0
+        self.mark = 0
 
     def visit_lecture(self):
-        pass
+        self.visited_lectures += 1
+        self._add_points(1)
 
     def do_homework(self):
-        pass
+        self.done_home_tasks += 2
+        self._add_points(2)
 
     def miss_lecture(self):
-        pass
+        self.missed_lectures -= 1
+        self._subtract_points(1)
 
     def miss_homework(self):
-        pass
+        self.missed_home_tasks -= 2
+        self._subtract_points(2)
 
-    def _add_points(self, points: int):
-        pass
+    def _add_points(self, pts: int):
+        if self.mark + pts > 10:
+            self.mark = 10
+        else:
+            self.mark += pts
 
-    def _subtract_points(self, points):
-        pass
+    def _subtract_points(self, pts):
+        if self.mark - pts < 0:
+            self.mark = 0
+        else:
+            self.mark -= pts
 
     def is_passed(self):
-        pass
+        gap = PASSING_GRADE - self.mark
+        if gap <= 0:
+            print("Good job!")
+        else:
+            print(f"You need to get {gap} more points. Try to do your best!")
 
     def __str__(self):
         status = (
